eec44439bd238371f3feddff79ac58015e9590d0
Jonathan Lee <jonathanhlee8@gmail.com>
2022-09-08 18:45:00 -0700

    adding changes to js html

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var cities = [];
 
-var cityFormEl=document.querySelector("#city-search-form");
-var cityInputEl=document.querySelector("#city");
-var weatherContainerEl=document.querySelector("#current-weather-container");
+var cityFormEl=document.querySelector("#city-search");
+var cityInputEl=document.querySelector("#city-name");
+var weatherContainerEl=document.querySelector("#current-container");
 var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
-var forecastContainerEl = document.querySelector("#fiveday-container");
-var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+var forecastContainerEl = document.querySelector("#fiveday");
+var pastSearchButtonEl = document.querySelector("#past-search");
 
 var formSubmitHandler = function(event){
     event.preventDefault();
